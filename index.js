b5b7d333bae15c343c794be527c7c778a34214cb
virtual PC <huxinsheng2015@icloud.com>
2025-04-22 11:24:49 +0800

    项目要求

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// Your project API endpoints
+// the requriements of this proj.
+// 1. must commit a address of yourself proj, not a sample of freecodecamp
+// 2. post a URL to /api/shorturl and get a response likes { original_url: 'http://www.freecodecamp.com', short_url: 1 }
+// 3. when you visit /api/shorturl/<short_url>, you will be redirected to the original_url
+// 4. if you visit the original URL which is invalid (not like www.freecodecamp.com), you will get a response likes { error: 'invalid url' }
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
